a731467e9e7243f9cc5a3df287cd955942932e4e
alejogm0520 <agomez13@eafit.edu.co>
2017-02-02 11:22:31 -0500

    Update Network.sh

--- Network.sh
+++ Network.sh
@@ -5,10 +5,14 @@ sudo dnf install R -y
 sudo pip install jupyter
 sudo dnf install scala -y
 
-
-
-
-
+# Instalando Spark 
+cd ..
+wget http://d3kbcqa49mib13.cloudfront.net/spark-2.1.0-bin-hadoop2.7.tgz
+tar xvf spark-2.1.0-bin-hadoop2.7.tgz
+rm  spark-2.1.0-bin-hadoop2.7.tgz
+sudo mv spark-2.1.0-bin-hadoop2.7 /usr/local/spark
+echo "export PATH=\$PATH:/usr/local/spark/bin" >> .bashrc
+source ~/.bashrc
 # Instalando Gephi
 
 wget https://github.com/gephi/gephi/releases/download/v0.9.1/gephi-0.9.1-linux.tar.gz
@@ -17,7 +21,6 @@ rm gephi-0.9.1-linux.tar.gz
 cd gephi-0.9.1/
 
 chmod +x bin/gephi
-./bin/gephi
 wget https://gephi.org/images/badge/logo_about_2.png
 
 # Creando Icono y acceso para Gephi
@@ -32,21 +35,10 @@ echo "Icon=/home/"${USER}"/gephi-0.9.1/logo_about_2.png" >> Gephi.desktop
 chmod +x Gephi.desktop
 mv Gephi.desktop /home/${USER}/Escritorio
 
-
-
-# Instalando Spark 
-cd ..
-wget http://d3kbcqa49mib13.cloudfront.net/spark-2.1.0-bin-hadoop2.7.tgz
-tar xvf spark-2.1.0-bin-hadoop2.7.tgz
-rm  spark-2.1.0-bin-hadoop2.7.tgz
-sudo mv spark-2.1.0-bin-hadoop2.7 /usr/local/spark
-echo "export PATH=\$PATH:/usr/local/spark/bin" >> .bashrc
-source ~/.bashrc
-
 # Instalando Neo4j
 
 wget https://neo4j.com/artifact.php?name=neo4j-community-3.1.1-unix.tar.gz
 tar -xf artifact.php\?name\=neo4j-community-3.1.1-unix.tar.gz
 rm artifact.php\?name\=neo4j-community-3.1.1-unix.tar.gz
-echo "export PATH=\$PATH:/home/\${USER}/neo4j-community-3.1.1/bin" >> .bashrc
+echo "export PATH=\$PATH:/home/${USER}/neo4j-community-3.1.1/bin" >> .bashrc
 source ~/.bashrc
